519ce6fff771cbf6d3e20b223823710476c1c89c
ossete <mehmetakifsakarya@gmail.com>
2020-05-04 19:10:06 +0200

    theme function added - testing phase started

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+console.log(body);
+
+function theme() {
+const body = document.querySelector('body');
+body.classList.add('dark');
+} 
+function themeBasic() {
+const body = document.querySelector('body');
+body.classList.remove('dark');
+} 
